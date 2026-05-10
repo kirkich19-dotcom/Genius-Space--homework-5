94fbff470e08f433a5db38f64eb9a581821e4246
kirkich19-dotcom <kirkich19@gmail.com>
2026-05-10 23:43:29 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,67 @@
+//---task-1---//
+
+const num = 0; //(enter numbers: 0, -3, 1)
+if (num > 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+//---task-2---//
+
+ const value = "test"; //(try wryting: "qwerty", true)
+ if (value === 'test') {
+     console.log(true);
+ } else {
+    console.log(false);
+};
+ 
+//---task-3---//
+
+const task3Num = 10;
+if (task3Num > 10) {
+    console.log(task3Num - 5);
+} else {
+    console.log(task3Num + 5);
+}
+
+//---task-4---//
+
+let month = prompt("Введіть порядковий номер міcяця"); // (try entering numbers from 1 to 12)
+if (month == 1) {
+    console.log("January");
+} else if (month == 2){
+    console.log("February");
+}else if (month == 3){
+    console.log("March");
+}else if (month == 4){
+    console.log("April");
+}else if (month == 5){
+    console.log("May");
+}else if (month == 6){
+    console.log("June");
+}else if (month == 7){
+    console.log("July");
+}else if (month == 8){
+    console.log("August");
+}else if (month == 9){
+    console.log("September");
+}else if (month == 10){
+    console.log("Oktober");
+}else if (month == 11){
+    console.log("November");
+}else if (month == 12){
+    console.log("Desember");
+}
+
+//---task-5---//
+let numOne = prompt("Введіть число від 1 до 9 (етап 1)"); // (try entering numbers from 1 to 9)
+let numTwo = prompt("Введіть число від 1 до 9 (етап 2)"); // (try entering numbers from 1 to 9)
+let numThree = prompt("Введіть число від 1 до 9 (етап 3)"); // (try entering numbers from 1 to 9)
+const summa = (numOne * numTwo * numThree);
+console.log(summa);
+
+
+
+
+
